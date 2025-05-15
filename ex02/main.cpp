7368c45e5c49dd028d6b9823422ec64616dd0ab8
AgarOther <agarother@gmail.com>
2025-05-15 11:57:05 +0200

    Added more tests

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,17 +6,19 @@
 /*   By: scraeyme <scraeyme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 15:35:24 by scraeyme          #+#    #+#             */
-/*   Updated: 2025/05/14 17:08:44 by scraeyme         ###   ########.fr       */
+/*   Updated: 2025/05/15 11:56:55 by scraeyme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 #include <iostream>
 #include <stack>
+#include <list>
 #include <iterator>
 
 int main()
 {
+	std::cout << "--------- STACK ----------" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -26,7 +28,6 @@ int main()
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
@@ -38,5 +39,27 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << "--------- LIST ----------" << std::endl;
+
+	std::list<int> list;
+	list.push_back(5);
+	list.push_back(17);
+	std::cout << list.back() << std::endl;
+	list.pop_back();
+	std::cout << list.size() << std::endl;
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+	std::list<int>::iterator it1 = list.begin();
+	std::list<int>::iterator ite2 = list.end();
+	++it1;
+	--it1;
+	while (it1 != ite2)
+	{
+		std::cout << *it1 << std::endl;
+		++it1;
+	}
 	return 0;
 }
